9f3782fe46c24abbdd81f3b74b00f843d9d568e0
BellowingButcher <100314347+BellowingButcher@users.noreply.github.com>
2024-12-05 06:56:35 -0500

    changed varriable inside of appendChild function to match new post varraible name

--- script.js
+++ script.js
@@ -25,5 +25,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
 		<p>Posted on: <time datetime="2024-12-04">December 4, 2024</time></p>
 		<p>Today I signed up for X and now I am writing blogs. What will happen next?</p>
 	`;
-	blogSection.appendChild(newPost);
+	blogSection.appendChild(anotherPost);
 });
